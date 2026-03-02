dd5de86961f823ad7fd212dc0bf82ead8578a5ce
as58201 <as5820@srmist.edu.in>
2026-03-02 23:24:22 +0530

    Update UC3.java

---
+++ UC3.java
@@ -0,0 +1,11 @@
+class UC3.java{
+	public static void main(String[] args) {
+	System.out.println(String.join(" ", " ** ", " ** ", " ** ", " **** "));
+	System.out.println(String.join(" ", " *  * ", " *  * ", " *    * ", " *        "));
+	System.out.println(String.join(" ", " *  * ", " *  * ", " *    * ", " *        "));
+	System.out.println(String.join(" ", " *  * ", " *  * ", " ** ", " **** "));
+	System.out.println(String.join(" ", " *  * ", " *  * ", " *      ", "        * "));
+	System.out.println(String.join(" ", " *  * ", " *  * ", " *      ", "        * "));
+	System.out.println(String.join(" ", " ** ", " ** ", " *      ", " **** "));
+	}
+}
